453b11f91013b633c591509505a56e055cada98b
SalikhovNail <salnail35@gmail.com>
2023-01-17 22:39:23 +0400

    First commit

---
+++ script/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// alert("Говорят ты хочешь senioroм стать?");
+
+// const result = confirm('Станешь Senior`ом?');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18 лет?', '');
+// console.log(answer);
+
+const answers = [];
+answers[0] = prompt("Как ваше имя?", "");
+answers[1] = prompt("Как ваша фамилия?", "");
+answers[2] = prompt("Как ваше ничего?", "");
+console.log(answers);
