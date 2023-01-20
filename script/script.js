d04383711306441a6ba00b1679011400ed7a78e1
SalikhovNail <salnail35@gmail.com>
2023-01-20 19:03:58 +0400

    First part

--- script/script.js
+++ script/script.js
@@ -1,15 +1,18 @@
 "use strict";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
 
-// alert("Говорят ты хочешь senioroм стать?");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// const result = confirm('Станешь Senior`ом?');
-// console.log(result);
-
-// const answer = prompt('Вам есть 18 лет?', '');
-// console.log(answer);
-
-const answers = [];
-answers[0] = prompt("Как ваше имя?", "");
-answers[1] = prompt("Как ваша фамилия?", "");
-answers[2] = prompt("Как ваше ничего?", "");
-console.log(answers);
+console.log(personalMovieDB);
